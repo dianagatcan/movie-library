4b6a44f395b190c2c3fd7a6790d97975b8fa853d
Diana G <67389035+dianagatcan@users.noreply.github.com>
2023-02-02 16:46:55 +0200

    add pagination

--- src/components/Container/container.jsx
+++ src/components/Container/container.jsx
@@ -1,109 +1,152 @@
 import { useEffect, useState } from 'react';
 import './container.scss';
 
+import { ArrowCircleLeft, ArrowCircleRight } from 'kidogo/duo';
+
 import Navbar from '../Navbar/navbar';
 import Modal from '../Modal/modal';
 import MovieCard from '../MovieCard/movieCard';
 
 const Container = () => {
-
-    const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState()
-    const [category, setCategory] = useState("top_rated")
-    const [modalMovie, setModalMovie] = useState({})
+    const [totalPages, setTotalPages] = useState();
+    const [category, setCategory] = useState('top_rated');
+    const [modalMovie, setModalMovie] = useState({});
+    const [showModal, setShowModal] = useState(false);
 
-    const [showModal, setShowModal] = useState(false)
+    const URL = 'https://api.themoviedb.org/3';
+    const SEARCH = (page, category) =>
+        URL + `/movie/${category}?` + API_KEY + `&language=en&page=${page}`;
+    const searchURL = (page) =>
+        URL + '/search/movie?' + API_KEY + `&language=en&page=${page}`;
 
-    const openModal = (movie) => {
-        setModalMovie(movie)
-        setShowModal(true)
-        document.body.classList.add('disable_scroll')
-    }
-
-    const closeModal = () => {
-        setShowModal(false)
-        document.body.classList.remove('disable_scroll')
-    }
-
-
-    const URL = 'https://api.themoviedb.org/3'
-    const SEARCH = (page, category) => URL + `/movie/${category}?` + API_KEY + `&language=en&page=${page}`;
-
-    const searchURL = (page) => URL + '/search/movie?' + API_KEY + `&language=en&page=${page}`;
-
-
-
-    function getMovies(url){
+    function getMovies(url) {
         const options = {
             method: 'GET',
             headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            }
+                'Content-Type': 'application/json;charset=utf-8',
+            },
         };
-        
-        fetch(url, options)
-            .then(response => response.json())
-            .then(data => {
-                console.log()
-                setMovies(data.results)
-                setTotalPages(data.total_pages)
-            })
-            .catch(err => console.error(err));
-    }
 
+        fetch(url, options)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log();
+                setMovies(data.results);
+                setTotalPages(data.total_pages);
+            })
+            .catch((err) => console.error(err));
+    }
 
     useEffect(() => {
-        getMovies(SEARCH(currentPage, category))
-    },[currentPage, category]) 
+        getMovies(SEARCH(currentPage, category));
+    }, [currentPage, category]);
 
     const sendToSearch = (search) => {
-        if(search) {
-          try{
-            getMovies(searchURL(currentPage) + '&query=' + search)
-        } catch(error){
-            console.error(error)
+        if (search) {
+            try {
+                getMovies(searchURL(currentPage) + '&query=' + search);
+            } catch (error) {
+                console.error(error);
+            }
         }
-        }
-    }
+    };
+
+    const openModal = (movie) => {
+        setModalMovie(movie);
+        setShowModal(true);
+        document.body.classList.add('disable_scroll');
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+        document.body.classList.remove('disable_scroll');
+    };
 
     const nextPage = () => {
-        if(currentPage < totalPages){
-            setCurrentPage(currentPage + 1)
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
         }
-    }
+    };
 
     const prevPage = () => {
-        if(currentPage > 1){
-            setCurrentPage(currentPage - 1)
-            console.log(currentPage)
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+            console.log(currentPage);
         }
-    }
+    };
 
+    const changeCategory = (category) => {
+        setCategory(category);
+        setCurrentPage(1);
+    };
 
-    return(
+    return (
         <section>
-            <Modal modalMovie={modalMovie} showModal={showModal} closeModal={closeModal} />
-            <Navbar sendToSearch={sendToSearch}/>
-            <section className={`${showModal ? 'main__modal' : ''} main`}>   
-                <section className='button_section'>
-                    <button className='button' onClick={() => setCategory("top_rated")}>Top rated</button>
-                    <button className='button' onClick={() => setCategory("popular")}>Popular</button>
-                    <button className='button' onClick={() => setCategory("now_playing")}>Now Playing</button>
+            <Modal
+                modalMovie={modalMovie}
+                showModal={showModal}
+                closeModal={closeModal}
+            />
+            <Navbar sendToSearch={sendToSearch} />
+            <section className={`${showModal ? 'main__modal' : ''} main`}>
+                <section className="category_section">
+                    <button
+                        className="category"
+                        onClick={() => changeCategory('top_rated')}
+                    >
+                        Top rated
+                    </button>
+                    <button
+                        className="category"
+                        onClick={() => changeCategory('popular')}
+                    >
+                        Popular
+                    </button>
+                    <button
+                        className="category"
+                        onClick={() => changeCategory('now_playing')}
+                    >
+                        Now Playing
+                    </button>
                 </section>
-                <button onClick={() => prevPage()}>Prev</button>
-                <button onClick={() => nextPage()}>Next</button>
-                <ul>{movies.map((movie) => {    
-                    return <MovieCard
-                    key={movie.id}
-                    movie={movie}
-                    openModal={openModal}
-                />
-                })}
+                <section className="page_section">
+                    <ArrowCircleLeft
+                        width="1.5rem"
+                        height="1.5rem"
+                        onClick={() => prevPage()}
+                        color="#404258"
+                        secondaryColor={
+                            currentPage === 1 ? '#6B728E' : '#EEEEEE'
+                        }
+                        cursor={currentPage === 1 ? '' : 'pointer'}
+                    />
+                    <ArrowCircleRight
+                        width="1.5rem"
+                        height="1.5rem"
+                        onClick={() => nextPage()}
+                        color="#404258"
+                        secondaryColor={
+                            currentPage === totalPages ? '#6B728E' : '#EEEEEE'
+                        }
+                        cursor={currentPage === totalPages ? '' : 'pointer'}
+                    />
+                </section>
+                <ul>
+                    {movies.map((movie) => {
+                        return (
+                            <MovieCard
+                                key={movie.id}
+                                movie={movie}
+                                openModal={openModal}
+                            />
+                        );
+                    })}
                 </ul>
             </section>
         </section>
-    )
-}
+    );
+};
 
 export default Container;
